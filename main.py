d00d8885daf26b7ae2e0e6eefb9f30c25a347417
Yann Le Dreff <yaledreff@gmail.com>
2022-07-18 14:04:45 +0200

    test

--- main.py
+++ main.py
@@ -3,23 +3,23 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import joblib
+# import joblib
 import numpy as np
 import uvicorn
-import os
-import io
-import sys
-import inspect
-from PIL import Image
+# import os
+# import io
+# import sys
+# import inspect
+# from PIL import Image
 from fastapi import FastAPI
-from fastapi import File
-from fastapi import UploadFile
-import cv2
+# from fastapi import File
+# from fastapi import UploadFile
+# import cv2
 
-from starlette.responses import StreamingResponse
-
-from MdeeplabV3 import *
-from utils import *
+# from starlette.responses import StreamingResponse
+#
+# from MdeeplabV3 import *
+# from utils import *
 
 app = FastAPI()
 
@@ -29,5 +29,5 @@ def read_root():
 
     # return StreamingResponse(content=imgio, media_type="image/jpeg")
 
-if __name__ == "__main__":
-    uvicorn.run("main:app")
+# if __name__ == "__main__":
+#     uvicorn.run("main:app")
