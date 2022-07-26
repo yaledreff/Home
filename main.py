8755b8feadb3eca086e4480b38e0e4893aacaf63
Yann Le Dreff <yaledreff@gmail.com>
2022-07-26 09:12:56 +0200

    Split API in 2 main POST method : /mask -> Get colored mask segmentation from input image /boxes -> Get colored bounding boxes overlay over input image

--- main.py
+++ main.py
@@ -3,21 +3,14 @@
 # Press Maj+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import joblib
-import numpy as np
 import uvicorn
-import os
-import io
-import sys
-import inspect
 from PIL import Image
 from fastapi import FastAPI
 from fastapi import File
 from fastapi import UploadFile
-import cv2
+
 
 from starlette.responses import StreamingResponse
-from MdeeplabV3 import *
 from utils import *
 
 app = FastAPI()
@@ -26,31 +19,27 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome from the API"}
 
-@app.post("/score")
-def get_image(file: UploadFile = File(...)):
+@app.post("/mask")
+def get_mask(file: UploadFile = File(...)):
     # chargement de l'image
     imageSrc = Image.open(file.file)
-    imageArray = np.asarray(imageSrc)
-    image = cv2.resize(imageArray, (512, 512))
-    image = image.reshape((1, 512, 512, 3))
-
-    # chargement du modèle de traitement (Deeplab V3) avec tous ses layers
-    model = getDeeplabV3(n_classes=8)
-
-    # chargement des poids calculés lors de l'entrainement
-    model.load_weights('model/DeeplabV3WeightsRUN.h5')
-
-    # On utilise le modèle pour prédire le mask de l'image source
-    imagePred = np.argmax(model.predict(image), -1)[0].reshape((512, 512))
+    imageRes = processImage(imageSrc)
     # Reconstitution d'une image en RGB pour affectation de couleurs facilement visibles à l'écran
-    imagePred3D = np.stack((imagePred, imagePred, imagePred), axis=2)
+    imagePred3D = np.stack((imageRes, imageRes, imageRes), axis=2)
     imagePredColored = getColoredMask(imagePred3D)
     imagePredColored = np.array(imagePredColored, dtype='uint8')
+    imagePredColored = cv2.resize(imagePredColored, (512, 256), interpolation=cv2.INTER_AREA)
+    return StreamingResponse(getBufferImage(Image.fromarray(imagePredColored, 'RGB')), media_type="image/jpeg")
 
-    imageRes = cv2.resize(imagePredColored, (512, 256), interpolation=cv2.INTER_AREA)
-    # imagePredColored = cv2.resize(imagePredColored, (256, 512), interpolation=cv2.INTER_AREA)
-    res, im_png = cv2.imencode(".png", imageRes)
-    return StreamingResponse(io.BytesIO(im_png.tobytes()), media_type="image/png")
+@app.post("/boxes")
+def get_boxes(file: UploadFile = File(...)):
+    # chargement de l'image
+    imageSrc = Image.open(file.file)
+    imageMaskRes = processImage(imageSrc)
+    imageSrc = cv2.resize(np.asarray(imageSrc), (512, 512))
+    imageMaskRes = cv2.resize(imageMaskRes, (512, 512))
+    imageWithBoxes = imageBoxesDbg(imageSrc, imageMaskRes, alpha=0.4)
+    return StreamingResponse(getBufferImage(Image.fromarray(imageWithBoxes, 'RGB')), media_type="image/jpeg")
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
--- utils.py
+++ utils.py
@@ -1,3 +1,10 @@
+import cv2
+import numpy as np
+import skimage.morphology as mph
+from skimage.measure import label, regionprops
+import io
+from MdeeplabV3 import *
+
 color_map = {
  '0': [0, 0, 0],
  '1': [153, 153, 0],
@@ -12,17 +19,72 @@ color_map = {
 def getColoredMask(image):
  sourcesColors = [(0, 0, 0), (1, 1, 1), (2, 2, 2), (3, 3, 3), (4, 4, 4), (5, 5, 5), (6, 6, 6), (7, 7, 7)]
  targetColors = [(0, 0, 0), (153, 153, 0), (255, 204, 204), (255, 0, 127), (0, 255, 0), (0, 204, 204), (255, 0, 0), (0, 0, 255)]
-
  imageSAV = image
 
  for i in range(0, 8):
- # i=7
-  print(i)
   sr, sg, sb = sourcesColors[i]
-  print(sourcesColors[i])
   tr, tg, tb = targetColors[i]
-  print(targetColors[i])
   red, green, blue = image[:, :, 0], image[:, :, 1], image[:, :, 2]
   mask = (red == sr) & (green == sg) & (blue == sb)
-  image[:, :, :3][mask] = [tb, tg, tr]
- return imageSAV
+  image[:, :, :3][mask] = [tr, tg, tb]
+ return image
+
+def imageBoxes(image, maskPredict, alpha=0.5):
+    def isIn(val, target):
+        return target == val
+
+    vFunc = np.vectorize(isIn)
+    imageSrc = image
+    imageColored = imageSrc
+    overlay = imageSrc.copy()
+    for i in [0, 5, 2, 1, 3, 4, 7, 6]:
+        mask = vFunc(maskPredict, i)
+        lbl = mph.label(mask)
+        props = regionprops(lbl)
+        for prop in props:
+            cv2.rectangle(overlay, (prop.bbox[1], prop.bbox[0]), (prop.bbox[3], prop.bbox[2]), color_map[str(i)],
+                      cv2.FILLED)
+
+    return cv2.addWeighted(overlay, alpha, imageColored, 1 - alpha, 0)
+
+def imageBoxesDbg(image, maskPredict, alpha=0.4):
+    def isIn(val, target):
+        return target == val
+
+    vFunc = np.vectorize(isIn)
+    imageSrc = image
+    imageColored = imageSrc
+    overlay = imageSrc.copy()
+    for i in [0, 5, 2, 1, 3, 4, 7, 6]:
+        mask = vFunc(maskPredict, i)
+        lbl = mph.label(mask)
+        props = regionprops(lbl)
+        for prop in props:
+            print(i)
+            cv2.rectangle(overlay, (prop.bbox[1], prop.bbox[0]), (prop.bbox[3], prop.bbox[2]), color_map[str(i)],
+                      cv2.FILLED)
+
+    return cv2.addWeighted(overlay, alpha, imageColored, 1 - alpha, 0)
+
+def get_bytes_value(image):
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='JPEG')
+    return img_byte_arr.getvalue()
+
+def getBufferImage(image):
+    filtered_image = io.BytesIO()
+    image.save(filtered_image, "JPEG")
+    filtered_image.seek(0)
+    return filtered_image
+
+def processImage(imageSrc):
+    imageArray = np.asarray(imageSrc)
+    image = cv2.resize(imageArray, (512, 512))
+    image = image.reshape((1, 512, 512, 3))
+    # chargement du modèle de traitement (Deeplab V3) avec tous ses layers
+    model = getDeeplabV3(n_classes=8)
+    # chargement des poids calculés lors de l'entrainement
+    model.load_weights('model/DeeplabV3WeightsRUN.h5')
+    # On utilise le modèle pour prédire le mask de l'image source
+    imagePred = np.argmax(model.predict(image), -1)[0].reshape((512, 512))
+    return imagePred
